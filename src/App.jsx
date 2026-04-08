59097fce5fa4e3c7f28dc13b8ef9683fc568627a
Sicelo-Vanyelwa <smvanyelwa@gnmail.com>
2026-04-08 18:39:27 +0200

    removing the added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar.jsx';
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -8,10 +8,6 @@ import TreasurerDashboard from './components/TreasurerDashboard.jsx';
 import UserDashboard from './components/UserDashboard';
 import './App.css';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
-import { useRef } from 'react';
-import { db, auth } from './Firebase-Config';
-import { collection, addDoc } from 'firebase/firestore';
-import {AddContribution} from './pages/AddContribution';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const { user, role } = useAuth();
@@ -30,29 +26,41 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          <Route path="/admin" element={
-            <ProtectedRoute allowedRoles={['admin']}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          } />
-          
-          <Route path="/treasurer" element={
+          <Route
+            path="/admin"
+            element={(
+              <ProtectedRoute allowedRoles={['admin']}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            )}
+          />
 
-            <ProtectedRoute allowedRoles={['treasurer', 'admin']}>
-              <TreasurerDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/dashboard" element={
-            <ProtectedRoute allowedRoles={['user', 'treasurer', 'admin']}>
-              <UserDashboard />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/treasurer"
+            element={(
+              <ProtectedRoute allowedRoles={['treasurer', 'admin']}>
+                <TreasurerDashboard />
+              </ProtectedRoute>
+            )}
+          />
 
-          <Route path="/add-contribution" element={
-            <ProtectedRoute allowedRoles={['user', 'treasurer', 'admin']}>
-              <AddContribution />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/dashboard"
+            element={(
+              <ProtectedRoute allowedRoles={['user', 'treasurer', 'admin']}>
+                <UserDashboard />
+              </ProtectedRoute>
+            )}
+          />
+
+          <Route
+            path="/add-contribution"
+            element={(
+              <ProtectedRoute allowedRoles={['user', 'treasurer', 'admin']}>
+                <AddContribution />
+              </ProtectedRoute>
+            )}
+          />
 
           <Route path="/unauthorized" element={<h2>Access Denied</h2>} />
         </Routes>
