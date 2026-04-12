665226f4f84bfa0e8d8afcf79f912abf11ea0592
Sicelo-Vanyelwa <smvanyelwa@gnmail.com>
2026-04-12 21:15:56 +0200

    fixing query code to use names to get contributions

--- src/pages/Thingsdata.jsx
+++ src/pages/Thingsdata.jsx
@@ -1,17 +1,26 @@
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {doc,getDoc, collection, query, where, getDocs } from "firebase/firestore";
 import { auth } from "../firebase/firebase";
 import { db } from "../firebase/firebase";
 
 export const fetchPaidContributions = async () => {
-  const user = auth.currentUser;
+const user = auth.currentUser;
 
   if (!user) {
     throw new Error("User not logged in");
   }
-
+console.log("Current user email:", user.email); // Debugging line to check user email
   // adjust this field to match your Firestore schema
-  const member = user.email;
-
+const userRef = query(
+  collection(db, "users"),
+   where("email", "==", user.email)
+);
+const userSnap = await getDocs(userRef);
+  
+if (userSnap.empty) {
+  throw new Error("User profile does not exist in Firestore");
+}
+const userData = userSnap.docs[0].data();
+const member = userData.name; // ✅ correct
   const contributionsRef = collection(db, "contributions");
 
   const q = query(
